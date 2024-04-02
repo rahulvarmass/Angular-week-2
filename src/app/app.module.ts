f05d38efc1232258e4c8a80ef73b0492ed9cd94b
rahulvarmass <rahulvarmass2002@gmail.com>
2024-04-02 17:09:22 +0530

    changes done in demobranch(routing)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CoursesComponent } from './courses/courses.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-import { CoursesComponent } from './courses/courses.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
+
+
+const appRoute: Routes =[
+  {path: '', component: HomeComponent},
+  {path: 'Home', component: HomeComponent},
+  {path: 'About', component: AboutComponent},
+  {path: 'Contact', component: ContactComponent},
+  {path: 'Courses', component: CoursesComponent},
+  {path: '**', component: ErrorComponent}
+]
 
 @NgModule({
   declarations: [
@@ -14,11 +26,13 @@ import { CoursesComponent } from './courses/courses.component';
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    CoursesComponent
+    CoursesComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
