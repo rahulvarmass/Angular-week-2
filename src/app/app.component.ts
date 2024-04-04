96a63d1cab62f0e0615244a686feb4ce8b518262
rahulvarmass <rahulvarmass2002@gmail.com>
2024-04-04 16:22:23 +0530

    first 5 videos of reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,10 +7,34 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-week-2';
 
-  onSubmit(form: NgForm){
-    console.log('form ');
+  title = 'angular-week-2';
+  defaultCountry= 'india';
+  firstname!: string;
+  lastname!: string;  
+  @ViewChild('myForm') form !: NgForm;
+
+  gender=[
+    {id:1, value:'Male'},
+    {id:2, value:'Female'},
+    {id:3, value: 'Other'}
+  ]
+
+  onSubmit(){
+    console.log(this.form);
   }
 
+
+  setDefaultValues() {
+    
+
+  this.form.form.patchValue({
+    personDetails:{
+      firstname:'Hashinie',
+      lastname:'Rahul Varma',
+      email:'srivaari@gmail.com'
+    }
+  })
+
+}
 }
