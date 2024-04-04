fc78955e6c519b0603a5daf1ec575522f8034c12
rahulvarmass <rahulvarmass2002@gmail.com>
2024-04-04 18:59:20 +0530

    reactive form 6 videos completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,11 @@ export class AppComponent {
   defaultCountry= 'india';
   firstname!: string;
   lastname!: string;  
-  @ViewChild('myForm') form !: NgForm;
+  email!: string;
+  gen!: string;
+  genderValue: string= ''
+  country!: string;
+  @ViewChild('myForm') form!: NgForm;
 
   gender=[
     {id:1, value:'Male'},
@@ -22,6 +26,15 @@ export class AppComponent {
 
   onSubmit(){
     console.log(this.form);
+
+    this.firstname= this.form.value.personDetails.firstname;
+    this.lastname= this.form.value.personDetails.lastname;
+    this.email= this.form.value.personDetails.email;
+    this.gen= this.form.value.personDetails.gender;
+    this.country= this.form.value.personDetails.country;
+    this.form.reset();
+
+
   }
 
 
@@ -30,9 +43,9 @@ export class AppComponent {
 
   this.form.form.patchValue({
     personDetails:{
-      firstname:'Hashinie',
-      lastname:'Rahul Varma',
-      email:'srivaari@gmail.com'
+      firstname:'sudharshan',
+      lastname:'kamalesh',
+      email:'sabari@gamil.com'
     }
   })
 
